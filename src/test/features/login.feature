73011df1bf0bb7b0fb92dc5b2231b44869d17b37
Mhaque2000 <mahamoodulhaque@gmail.com>
2025-10-23 04:53:20 +0530

    addToCart

--- src/test/features/login.feature
+++ src/test/features/login.feature
@@ -6,3 +6,19 @@ When I login with the valid credentials
 And I clicked on the settings button
 And I clicked on the "logout" button
 Then I route back to the login page
+
+Scenario Outline: Login and Logout with invalid credentials
+Given I am on the login page
+When I login with the invalid credentials
+Then I should get the login failed "<message>"
+Examples:
+    | message |
+    | Username and password do not match any user in this service |
+
+Scenario Outline: Login and Logout without credentials
+Given I am on the login page
+When I login without credentials
+Then I should get the login failed "<message>"
+Examples:
+    | message |
+    | Username is required |
--- src/test/steps/loginSteps.ts
+++ src/test/steps/loginSteps.ts
@@ -10,6 +10,19 @@ When(/^I login with the valid credentials$/, async () => {
 	await pageFixture.page.locator('[name="password"]').fill('secret_sauce');
 	await pageFixture.page.locator('[type="submit"]').click();
 });
+When(/^I login with the invalid credentials$/, async () => {
+	await pageFixture.page.locator('[name="user-name"]').fill('invalid_user');
+	await pageFixture.page.locator('[name="password"]').fill('secret_sauce');
+	await pageFixture.page.locator('[type="submit"]').click();
+});
+When(/^I login without credentials$/, async () => {
+	await pageFixture.page.locator('[type="submit"]').click();
+});
+Then(/^I should get the login failed "([^"]*)"$/, async (expectedErrorMessage) => {
+	const errorMessageElement =  pageFixture.page.locator('h3[data-test="error"]');
+	const actualErrorMessage = await errorMessageElement.textContent();
+	expect(actualErrorMessage).toContain(expectedErrorMessage);
+});
 When(/^I clicked on the settings button$/, async () => {
 	await pageFixture.page.locator('.bm-burger-button').click();
 });
