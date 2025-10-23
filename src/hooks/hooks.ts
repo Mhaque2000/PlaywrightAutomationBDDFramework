b3a10594600da09d945251810b253dae249f180f
Mhaque2000 <mahamoodulhaque@gmail.com>
2025-10-23 06:23:28 +0530

    multiple-html-cucumber-report screenshot added after all steps

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,7 +1,7 @@
 import { Browser, BrowserContext, chromium, Page } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
 import test = require("node:test");
-import { After, AfterAll, Before, BeforeAll, Status } from "@cucumber/cucumber";
+import { After, AfterAll, AfterStep, Before, BeforeAll, Status } from "@cucumber/cucumber";
 import path = require("path");
 let browser: Browser;
 let context: BrowserContext
@@ -14,7 +14,7 @@ Before(async function(){
     page = await context.newPage();
     pageFixture.page = page;
 })
-After(async function({pickle, result}){
+AfterStep(async function({pickle, result}){
     //Screenshot only for failure
     // if(result?.status == Status.FAILED){
     //     const image = await pageFixture.page.screenshot({path: `./test-results/screenshots/${pickle.name}.png`, type:'png'})
@@ -22,6 +22,8 @@ After(async function({pickle, result}){
     // }
     const image = await pageFixture.page.screenshot({path: `./test-results/screenshots/${pickle.name}.png`, type:'png'})
     this.attach(image, 'image/png')
+})
+After(async function(){
     await page.close();
     await context.close();
 })
