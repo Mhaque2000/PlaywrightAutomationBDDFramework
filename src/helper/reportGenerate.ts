bff78bc6df637704fcab18b0210ad0b7388d1f0c
Mhaque2000 <mahamoodulhaque@gmail.com>
2025-10-23 06:00:47 +0530

    multiple-html-cuucmber-report

--- src/helper/reportGenerate.ts
+++ src/helper/reportGenerate.ts
@@ -1,17 +1,26 @@
 const report = require('multiple-cucumber-html-reporter');
- 
+const path = require('path');
+
 report.generate({
-    jsonDir: './test-results/report_advanced/',
-    reportPath: './test-results/report_advanced/',
-    metadata:{
+    jsonDir: path.join(__dirname, '..', '..', 'test-results'), // folder with cucumber JSON output
+    reportPath: path.join(__dirname, '..', '..', 'test-results', 'report_advanced'),
+    metadata: {
         browser: {
-            name: 'chrome',
-            version: '60'
+            name: 'chromium',    // Playwright default browser
+            version: 'latest'    // version doesn’t need to match exactly
         },
-        device: 'Local test machine',
+        device: 'GitHub Actions runner',
         platform: {
             name: 'ubuntu',
-            version: '16.04'
+            version: process.platform // optional, can leave as '22.04'
         }
+    },
+    customData: {
+        title: 'Project Info',
+        data: [
+            { label: 'Project', value: 'Project 2' },
+            { label: 'Release', value: '1.0.0' },
+            { label: 'Cycle', value: 'GitHub Actions CI' },
+        ]
     }
 });
